051f3fedc9e637c3f1a5da5d9160c5371fdee478
JaviThomas <dog2785@gmail.com>
2017-12-26 14:47:02 -0500

    Creates policy specifically for new sub-users

--- PolicyEditor.py
+++ PolicyEditor.py
@@ -1,6 +1,46 @@
-response = client.create_policy(
-    PolicyName='string',
-    Path='string',
-    PolicyDocument='string',
-    Description='string'
+import json
+
+import boto3
+
+# Create IAM client
+iam = boto3.client('iam')
+
+# Create a policy
+patient_adder_policy = {
+    "Version": "2012-10-17",
+    "Statement": [
+        {   
+            "Effect": "Allow",
+            "Action": [
+                "dynamodb:BatchGetItem",
+                "dynamodb:PutItem",
+                "dynamodb:DescribeTable",
+                "dynamodb:GetShardIterator",
+                "dynamodb:GetItem",
+                "dynamodb:Scan",
+                "dynamodb:DescribeStream",
+                "dynamodb:Query",
+                "dynamodb:GetRecords"
+            ],
+            "Resource": "arn:aws:dynamodb:us-east-1:308565516874:table/bob"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "dynamodb:DescribeReservedCapacityOfferings",
+                "dynamodb:ListTables",
+                "dynamodb:DescribeReservedCapacity",
+                "dynamodb:ListTagsOfResource",
+                "dynamodb:DescribeTimeToLive",
+                "dynamodb:DescribeLimits",
+                "dynamodb:ListStreams"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+response = iam.create_policy(
+  PolicyName='patientAdditionPolicy',
+  PolicyDocument=json.dumps(patient_adder_policy)
 )
+print(response)
